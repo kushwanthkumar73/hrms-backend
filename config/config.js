e39fa4ffbb6444ff6d9aa4290a0ada93f90761fb
kushwanthkumar73 <kushwanth2001@gmail.com>
2025-11-22 12:26:29 +0530

    Fix config for Render deployment

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -6,8 +6,23 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
-    port: process.env.DB_PORT
-  }
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
+
 
